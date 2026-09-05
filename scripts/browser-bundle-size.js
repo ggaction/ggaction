81225436461eec0e0298a29f98ca42cc569e6201
Hyeon Jeon <hyeonjeon@Hyeons-Mac-mini.local>
2026-09-05 20:58:05 +0900

    build: apply approved full bundle ceiling

--- scripts/browser-bundle-size.js
+++ scripts/browser-bundle-size.js
@@ -5,7 +5,7 @@ import path from "node:path";
 import { build } from "vite";
 
 export const BROWSER_BUNDLE_GZIP_LIMITS = Object.freeze({
-  ggaction: 235_000,
+  ggaction: 237_000,
   "ggaction/basic": 125_000,
   "ggaction/svg": 25_000
 });
